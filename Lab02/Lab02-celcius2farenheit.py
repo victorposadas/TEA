8d513c621ef4c0f2145294d4cb9b738c1119ecb9
Victor Emiliano Posadas Rivera (24215) <victor.posadas@est.zamorano.edu>
2022-09-08 09:05:12 -0600

    Laboratorio #2

---
+++ Lab02/Lab02-celcius2farenheit.py
@@ -0,0 +1,4 @@
+# Tendencia e Innovación en Tencología Agrícola (TEA)
+temperaturaCelcius = float(input("¿Cual es la temperatura en Celcius?"))
+temperaturaFarenheit = (9/5) * temperaturaCelcius +32
+print(temperaturaFarenheit)
